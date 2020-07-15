e732e9a559dc55f6d9ac6b5070cec612560b3def
895623741x <52303790+895623741x@users.noreply.github.com>
2020-07-15 08:46:11 -0700

    Add files via upload
    1. input finished
    2. controller finished

---
+++ starter/app.js
@@ -0,0 +1,122 @@
+//Budget Controller
+var budgetController = (function() {
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+
+    };
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+
+    };
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            //creat new id
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+
+            //creat new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            //push it into our data structure
+            data.allItems[type].push(newItem);
+
+
+            //return the new item
+            return newItem;
+        },
+        testing: function() {
+            console.log(data);
+        }
+    }
+})();
+
+//UI Controller
+var UIController = (function() {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };    
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        } 
+    };
+})();
+
+//Global App Controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', controllAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                controllAddItem();
+            }
+        });
+    };
+
+    
+
+    var controllAddItem = function() {
+        var input, newItem;
+        //1. get the field input data
+        input = UICtrl.getInput();
+        console.log(input);
+        //2.add the button to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        //3.add the item to the UI
+
+        //4.calculate the budget
+
+        //5.display the budget on the UI
+    };
+
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    };
+
+    
+})(budgetController, UIController);
+
+controller.init();
